136cbcbeb6101fb3a974ee7050e11f58ecbf68a8
ashish patil <patilas@visa.com>
2017-03-20 06:04:50 +0000

    alert messages

--- src/Image.js
+++ src/Image.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import swal from "sweetalert";
+import '../node_modules/sweetalert/dist/sweetalert.css';
+
 
 class Image extends React.Component {
+    constructor () {
+        super();
+        this.onClick = this.onClick.bind(this);
+        //set initial state
+        // this.state = {
+        //     correctAnswer : {
+        //
+        //     }
+        // }
+    }
+    
+    onClick = (e) => {
+        e.preventDefault();
+        let correct = e.currentTarget.alt === 'cat';
+        console.log("the target is ",e.currentTarget.alt);
+        if (correct)
+            swal("Good job!", "You clicked the button!", "success");
+        else
+            swal({
+                title: "Error!",
+                text: "Here's my error message!",
+                type: "error",
+                confirmButtonText: "Cool"
+            });
+    };
+    
     render() {
         const path = `./images/${this.props.name}.png`;
         return (
             <div>
-                <img alt={this.props.name} className="img-thumbnail" src={path}/>
+                <img alt={this.props.name} className="img-thumbnail" src={path} onClick={this.onClick}/>
             </div>
         );
     }
